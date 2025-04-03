2b897e9aa2a0e02055c5b819b6ad9afc383f3c9e
driplase <85550737+driplase@users.noreply.github.com>
2025-04-04 00:33:32 +0900

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   alias: {
-    "assets": "/<srcDir>/assets",
     "data": "/<srcDir>/data",
   },
   css: [
