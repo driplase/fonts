2fb9863909378b05bad088feabba11b0c0f44c93
Driplase <258vr.g.subspeed@gmail.com>
2025-04-03 22:51:12 +0900

    why is the css not working...

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
     "data": "/<srcDir>/data",
   },
   css: [
-    '/assets/main.css'
+    '~/assets/main.css'
   ],
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true }
