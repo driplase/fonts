d4ffe8066d372863f4627a143e9bc7068ff742a1
Driplase <258vr.g.subspeed@gmail.com>
2025-04-03 23:24:59 +0900

    revert...

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   alias: {
+    "assets": "/<srcDir>/assets",
     "data": "/<srcDir>/data",
   },
   css: [
-    '~/assets/main.css'
+    'assets/main.css'
   ],
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true }
